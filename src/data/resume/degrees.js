135637fafb697810dcda339fc87041c239944234
zklemily <ezhang1024@gmail.com>
2024-06-30 22:15:19 -0400

    change double quotes to single quotes

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,14 +1,14 @@
 const degrees = [
   {
-    school: "Stanford University",
-    degree: "M.S. Computational and Mathematical Engineering",
-    link: "https://stanford.edu",
+    school: 'Stanford University',
+    degree: 'M.S. Computational and Mathematical Engineering',
+    link: 'https://stanford.edu',
     year: 2016,
   },
   {
-    school: "University at Buffalo",
-    degree: "B.S. Electrical Engineering, Computer Engineering",
-    link: "https://buffalo.edu",
+    school: 'University at Buffalo',
+    degree: 'B.S. Electrical Engineering, Computer Engineering',
+    link: 'https://buffalo.edu',
     year: 2012,
   },
 ];
