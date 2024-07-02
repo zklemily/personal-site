33115a53c28a58c6ee8a5a550a7dcb28f676cef0
zklemily <ezhang1024@gmail.com>
2024-07-02 14:59:19 -0400

    change formatting in blog post

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -5,26 +5,26 @@ import Main from '../layouts/Main';
 import data from '../data/posts';
 
 const Blog = () => (
-    <Main title="Blog" description="Read about Kaili's ideas, reflection, and random thoughts.">
-        <article className="post" id="blogs">
-            <header>
-                <div className="title">
-                    <h2>
-                        <Link to="/blog">Blog</Link>
-                    </h2>
-                    <p>Read about Kaili's ideas, reflection, and random thoughts.</p>
-                </div>
-            </header>
-            <ul>
-                {data.map(post => (
-                    <li key={data.slug}>
-                        <Link to={`/blog/${data.slug}`}>{data.title}</Link>
-                        <p>{data.date}</p>
-                    </li>
-                ))}
-            </ul>
-        </article>
-    </Main>
+  <Main title="Blog" description="Read about Kaili's ideas, reflection, and random thoughts.">
+    <article className="post" id="blogs">
+      <header>
+        <div className="title">
+          <h2>
+            <Link to="/blog">Blog</Link>
+          </h2>
+          <p>Read about Kaili's ideas, reflection, and random thoughts.</p>
+        </div>
+      </header>
+      <ul>
+        {data.map((post) => (
+          <li key={data.slug}>
+            <Link to={`/blog/${data.slug}`}>{data.title}</Link>
+            <p>{data.date}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  </Main>
 );
 
 export default Blog;
--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -5,24 +5,24 @@ import Markdown from 'markdown-to-jsx';
 import Main from '../layouts/Main';
 
 const BlogPost = () => {
-    const { slug } = useParams();
-    const [content, setContent] = useState('');
+  const { slug } = useParams();
+  const [content, setContent] = useState('');
 
-    useEffect(() => {
-        import(`../posts/${slug}.md`)
-            .then(res => {
-                fetch(res.default)
-                    .then(response => response.text())
-                    .then(text => setContent(text));
-            })
-            .catch(err => console.error(err));
-    }, [slug]);
+  useEffect(() => {
+    import(`../posts/${slug}.md`)
+      .then((res) => {
+        fetch(res.default)
+          .then((response) => response.text())
+          .then((text) => setContent(text));
+      })
+      .catch((err) => console.error(err));
+  }, [slug]);
 
-    return (
-        <Main>
-            <Markdown>{content}</Markdown>
-        </Main>
-    );
+  return (
+    <Main>
+      <Markdown>{content}</Markdown>
+    </Main>
+  );
 };
 
 export default BlogPost;
