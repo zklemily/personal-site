739abb9c08eb3da4f13d8dc5b91317e1192770ec
zklemily <ezhang1024@gmail.com>
2024-07-03 21:58:01 -0400

    update filter button style

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,37 +4,47 @@ import { Link } from 'react-router-dom';
 import Main from '../layouts/Main';
 import { data, categories } from '../data/posts';
 import PostPreview from '../components/Blog/PostPreview';
-import Filter from '../components/Blog/Filter';
+
+import CategoryButton from '../components/Resume/Skills/CategoryButton';
 
 const Blog = () => {
-  const initialButtons = Object.fromEntries(
-    [['All', false]].concat(categories.map((name) => [name, false])),
-  );
+  const initialButtons = Object.fromEntries([['All', false]].concat(categories.map((category) => [category, false])));
 
   const [buttons, setButtons] = useState(initialButtons);
 
-  const handleCategoryChange = (label) => {
-    const newButtons = Object.keys(buttons).reduce(
-      (obj, key) => ({
-        ...obj,
-        [key]: label === key && !buttons[key],
-      }),
-      {},
-    );
-    newButtons.All = !Object.keys(buttons).some((key) => newButtons[key]);
+  const handleChildClick = (label) => {
+    const newButtons = Object.keys(buttons).reduce((obj, key) => ({
+      ...obj,
+      [key]: (label === key) && !buttons[key],
+    }), {});
+    newButtons.All = !Object.keys(newButtons).some((key) => newButtons[key]);
     setButtons(newButtons);
   };
 
-  const selectedCategories = Object.keys(buttons).filter(
-    (key) => buttons[key] && key !== 'All',
-  );
+  const getFilteredPosts = () => {
+    const activeCategory = Object.keys(buttons).reduce((cat, key) => (
+      buttons[key] ? key : cat
+    ), 'All');
 
-  const filteredPosts = selectedCategories.length
-    ? data.filter((post) => post.categories.some((cat) => selectedCategories.includes(cat)))
-    : data;
+    return data.filter((post) => (activeCategory === 'All' || post.categories.includes(activeCategory)))
+      .map((post) => (
+        <PostPreview key={post.slug} data={post} />
+      ));
+  };
+
+  const getButtons = () => Object.keys(buttons).map((key) => (
+    <CategoryButton
+      label={key}
+      key={key}
+      active={buttons}
+      handleClick={handleChildClick}
+    >
+      {key}
+    </CategoryButton>
+  ));
 
   return (
-    <Main title="Blog" description="What&apos;s on my mind">
+    <Main title="Blog" description="What's on my mind">
       <article className="post" id="blogs">
         <header>
           <div className="title">
@@ -44,16 +54,11 @@ const Blog = () => {
             <p>What&apos;s on my mind</p>
           </div>
         </header>
-        <Filter
-          categories={categories}
-          selectedCategories={selectedCategories}
-          onCategoryChange={handleCategoryChange}
-          buttons={buttons}
-        />
+        <div className="skill-button-container">
+          {getButtons()}
+        </div>
         <div className="blog-list">
-          {filteredPosts.map((post) => (
-            <PostPreview key={post.slug} data={post} />
-          ))}
+          {getFilteredPosts()}
         </div>
       </article>
     </Main>
